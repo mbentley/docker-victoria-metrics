49907d606e237b9865cb9df78fa7131ed114280b
Matt Bentley <mbentley@mbentley.net>
2025-11-02 00:11:19 -0400

    Updated to add v1 tagging
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- vm_tag_manifest.sh
+++ vm_tag_manifest.sh
@@ -9,6 +9,13 @@ tag_manifest() {
   # get expected tag from first argument
   EXPECTED_TAG="${1}"
 
+  # test if major.minor or just major
+  if ! echo "${EXPECTED_TAG}" | grep -q '\.'
+  then
+    # major only; set variable
+    MAJOR_ONLY=true
+  fi
+
   # get latest full version from GitHub releases
   echo -n "Getting full version for ${EXPECTED_TAG} from GitHub releases..."
   VM_VERSION="$(echo "${VM_RELEASES}" | grep "^${EXPECTED_TAG}\." | head -n 1)"
@@ -54,28 +61,35 @@ tag_manifest() {
 
   echo "done"
 
-  # get the target tag we want to use
-  #MAJOR_MINOR_TAG="$(echo "${VM_VERSION}" | awk -F 'v' '{print $2}' | awk -F '.' '{print $1"."$2}')"
-  MAJOR_MINOR_TAG="$(echo "${VM_VERSION}" | awk -F '.' '{print $1"."$2}')"
-
-  # check to see if we got a tag digest
-  if [ -z "${MAJOR_MINOR_TAG}" ]
+  # see if we want a MAJOR.MINOR or just MAJOR tag
+  if [ "${MAJOR_ONLY}" = "true" ]
   then
-    echo "ERROR: MAJOR_MINOR_TAG not set!"
+    # major only; get the destination tag we want to use
+    DESTINATION_TAG="$(echo "${VM_VERSION}" | awk -F '.' '{print $1}')"
+  else
+    # major.minor; get the destination tag we want to use
+    DESTINATION_TAG="$(echo "${VM_VERSION}" | awk -F '.' '{print $1"."$2}')"
+  fi
+
+  # check to see if we got a destination tag
+  if [ -z "${DESTINATION_TAG}" ]
+  then
+    echo "ERROR: DESTINATION_TAG not set!"
     exit 1
   fi
 
-  # check to see if the major.minor tag is no longer the value of EXPECTED_TAG
-  if [ "${MAJOR_MINOR_TAG}" != "${EXPECTED_TAG}" ]
+  # check to see if the tag is no longer the value of EXPECTED_TAG
+  if [ "${DESTINATION_TAG}" != "${EXPECTED_TAG}" ]
   then
-    echo "ERROR: the major.minor tag is no longer ${EXPECTED_TAG}; we found ${TRIMMED_TAG}!"
+    echo "ERROR: the tag is no longer ${EXPECTED_TAG}; we found ${TRIMMED_TAG}!"
     exit 1
   fi
 
   # create the new manifest and push the manifest to docker hub
   echo -n "Create new manifest and push to Docker Hub..."
-  docker buildx imagetools create --progress plain -t "mbentley/victoria-metrics:${MAJOR_MINOR_TAG}" "victoriametrics/victoria-metrics@${AMD64_TAG_DIGEST}" "victoriametrics/victoria-metrics@${ARM64_TAG_DIGEST}"
-  if [ "${MAJOR_MINOR_TAG}" == "${LATEST_MAJOR_MINOR_TAG}" ]
+  docker buildx imagetools create --progress plain -t "mbentley/victoria-metrics:${DESTINATION_TAG}" "victoriametrics/victoria-metrics@${AMD64_TAG_DIGEST}" "victoriametrics/victoria-metrics@${ARM64_TAG_DIGEST}"
+  # only tag latest for major.minor tags (not major-only tags)
+  if [ "${MAJOR_ONLY}" != "true" ] && [ "${DESTINATION_TAG}" == "${LATEST_MAJOR_MINOR_TAG}" ]
   then
     # also tag this as latest
     docker buildx imagetools create --progress plain -t "mbentley/victoria-metrics:latest" "victoriametrics/victoria-metrics@${AMD64_TAG_DIGEST}" "victoriametrics/victoria-metrics@${ARM64_TAG_DIGEST}"
@@ -88,8 +102,14 @@ tag_manifest() {
 GITHUB_TAGS="$(wget -q -O - "https://api.github.com/repos/VictoriaMetrics/VictoriaMetrics/tags?per_page=50")"
 
 # get the last five major.minor tags
-#EXPECTED_TAGS="$(echo "${GITHUB_TAGS}" | jq -r '.[]|.name' | awk -F 'v' '{print $2}' | awk -F '.' '{print $1 "." $2}' | sort --version-sort -ru | grep -v -- -cluster | head -n 5)"
-EXPECTED_TAGS="$(echo "${GITHUB_TAGS}" | jq -r '.[]|.name' | awk -F '.' '{print $1 "." $2}' | sort --version-sort -ru | grep -v -- -cluster | head -n 5)"
+#EXPECTED_MAJOR_MINOR_TAGS="$(echo "${GITHUB_TAGS}" | jq -r '.[]|.name' | awk -F 'v' '{print $2}' | awk -F '.' '{print $1 "." $2}' | sort --version-sort -ru | grep -v -- -cluster | head -n 5)"
+EXPECTED_MAJOR_MINOR_TAGS="$(echo "${GITHUB_TAGS}" | jq -r '.[]|.name' | awk -F '.' '{print $1 "." $2}' | sort --version-sort -ru | grep -v -- -cluster | head -n 5)"
+
+# set expected major tags from the major.minor list
+EXPECTED_MAJOR_TAGS="$(echo "${EXPECTED_MAJOR_MINOR_TAGS}" | tr " " "\n" | awk -F '.' '{print $1}' | sort -nu | xargs)"
+
+# combine list of all expected tags
+ALL_EXPECTED_TAGS="${EXPECTED_MAJOR_MINOR_TAGS} ${EXPECTED_MAJOR_TAGS}"
 
 # get the latest tag
 LATEST_MAJOR_MINOR_TAG="$(echo "${GITHUB_TAGS}" | jq -r '.[]|.name' | awk -F 'v' '{print $2}' | awk -F '.' '{print $1 "." $2}' | sort --version-sort -ru | grep -v -- -cluster | head -n 1)"
@@ -101,4 +121,4 @@ VM_RELEASES="$(echo "${GITHUB_TAGS}" | jq -r '.[]|.name' | grep -v -- -cluster |
 . "$(command -v env_parallel.bash)"
 
 # run multiple scans in parallel
-env_parallel --env tag_manifest --env VM_RELEASES --env LATEST_MAJOR_MINOR_TAG --halt soon,fail=1 -j 5 tag_manifest ::: "${EXPECTED_TAGS}"
+env_parallel --env tag_manifest --env VM_RELEASES --env LATEST_MAJOR_MINOR_TAG --env ALL_EXPECTED_TAGS --halt soon,fail=1 -j 5 tag_manifest ::: ${ALL_EXPECTED_TAGS}
